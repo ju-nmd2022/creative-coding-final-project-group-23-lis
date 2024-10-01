2152a40ec41dd343c3183c07292ecf0bc3c88206
SelmaPalmquist <pase22pc@student.ju.se>
2024-10-01 22:25:00 +0200

    Background and canvas alteration

--- main.js
+++ main.js
@@ -1,3 +1,47 @@
+let video;
+let handpose;
+let predictions = [];
+
+function preload() {
+  handpose = ml5.handPose(modelLoaded);
+}
+
+function setup() {
+  createCanvas(740, 580);
+  video = createCapture(VIDEO);
+  video.size(740, 580);
+  video.hide();
+
+  handpose.detectStart(video, getHandsData);
+}
+
+function getHandsData(results) {
+  predictions = results;
+}
+
+function draw() {
+  background(255, 255, 255);
+  image(video, 0, 0, 740, 580);
+
+  for (let hand of predictions) {
+    const keypoints = hand.keypoints;
+    for (let keypoint of keypoints) {
+      push();
+      noStroke();
+      fill(0, 255, 0);
+      ellipse(keypoint.x, keypoint.y, 10);
+      pop();
+    }
+  }
+}
+
+function modelLoaded() {
+  console.log("Model Loaded!");
+}
+
+
+
+/*
 let handpose;
 let video;
 let hands = [];
@@ -47,3 +91,5 @@ function draw() {
 function getHandsData(results) {
   hands = results;
 }
+
+*/

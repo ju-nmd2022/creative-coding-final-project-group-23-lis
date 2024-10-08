dc425ca4ea5dd18fce9c45067b2e9aefc14965d9
SelmaPalmquist <pase22pc@student.ju.se>
2024-10-08 15:52:34 +0200

    Impatient mood and array

--- main.js
+++ main.js
@@ -1,14 +1,11 @@
 let video;
 let handpose;
 let predictions = [];
-let sadArtist, happyArtist, angryArtist, misArtist, benArtist;
+let sadArtist, happyArtist, angryArtist, misArtist, benArtist, impatientArtist;
 let mood = "happy";
-let moodImageElement;
 let comment = document.getElementById("artist-comment");
-let headHeart, browsLift, eyesCross, noseSeptum, lipBite;
-let filterArray;
-
-
+let moodTimeout;
+let happyFilterArray = [];
 
 function preload() {
     handpose = ml5.handPose(modelLoaded);
@@ -19,16 +16,16 @@ function preload() {
     angryArtist = "images/artist-angry.PNG";
     misArtist = "images/artist-mis.PNG";
     benArtist = "images/artist-ben.PNG";
+    impatientArtist = "images/artist-impatient.PNG"; 
 
     //the facial features in the array
     headHeart = "images/head-heart.PNG";
     browsLift = "images/brows-lift.PNG";
     eyesCross = "images/eyes-cross.PNG";
-    noseSeptum = "images/noseSeptum.PNG";
-    lipBite = "images/lipBite.PNG";
-    filterArray = [headHeart, browsLift, eyesCross, noseSeptum, lipBite];
+    noseSeptum = "images/nose-septum.PNG";
+    lipBite = "images/lip-bite.PNG";
+    happyFilterArray = [headHeart, browsLift, eyesCross, noseSeptum, lipBite];
 }
-
 
 function setup() {
     createCanvas(740, 580);
@@ -38,20 +35,24 @@ function setup() {
   
     handpose.detectStart(video, getHandsData);
   
-    //randomize number for the mood
-    let randomizedMood = Math.floor(Math.random() * 8);
-    
-    //set mood based on randomized number
-    if (randomizedMood === 0 || randomizedMood === 1) {
+    randomizeMood();
+}
+
+//function to randomize the artist's mood
+function randomizeMood() {
+    let randomizedMood = Math.floor(Math.random() * 16); 
+
+    //set mood based on the randomized number
+    if (randomizedMood <= 3) {
       console.log("The artist is sad.");
       mood = "sad";
-    } else if (randomizedMood === 2 || randomizedMood === 3) {
+    } else if (randomizedMood <= 7) {
       console.log("The artist is angry.");
       mood = "angry";
-    } else if (randomizedMood === 4) {
+    } else if (randomizedMood <= 9) {
       console.log("The artist is feeling mischievous.");
       mood = "mischievous";
-    } else if (randomizedMood === 5) {
+    } else if (randomizedMood <= 11) {
       console.log("The artist is feeling benevolent.");
       mood = "benevolent";
     } else {
@@ -59,71 +60,105 @@ function setup() {
       mood = "happy";
     }
     
-  updateMoodImages();
+    updateMoodImages();
+
+    //changing the mood "impatient" after 20 seconds
+    moodTimeout = setTimeout(() => {
+        transitionToImpatient();
+    }, 20000); 
 }
-  
-  //function to hide or display images and test based on the current mood so the user 
-  //is made aware what mood the artist is in
-  function updateMoodImages() {
+
+//used the help of chatgpt to get this impatient part to work
+function transitionToImpatient() {
+    console.log("The artist has become impatient.");
+    mood = "impatient";
+    updateMoodImages();
+
+    //refresh the mood again after being impatient for 10 secs
+    moodTimeout = setTimeout(() => {
+        randomizeMood();
+    }, 10000);
+}
+
+//hide or display images and text based on the current mood
+function updateMoodImages() {
     document.getElementById("sad-image").style.display = "none";
     document.getElementById("angry-image").style.display = "none";
     document.getElementById("mischievous-image").style.display = "none";
     document.getElementById("benevolent-image").style.display = "none";
     document.getElementById("happy-image").style.display = "none";
-  
+    document.getElementById("impatient-image").style.display = "none"; // Hide impatient by default
+
+    //update the displayed image and comment based on the current mood
     if (mood === "sad") {
       document.getElementById("sad-image").style.display = "block";
-      comment.innerHTML = "Artist is sad."
-
+      comment.innerHTML = "Artist is sad.";
     } else if (mood === "angry") {
       document.getElementById("angry-image").style.display = "block";
-      comment.innerHTML = "Artist is angry."
-
+      comment.innerHTML = "Artist is angry.";
     } else if (mood === "mischievous") {
       document.getElementById("mischievous-image").style.display = "block";
-      comment.innerHTML = "Artist is feeling mischevious."
-
+      comment.innerHTML = "Artist is feeling mischievous.";
     } else if (mood === "benevolent") {
       document.getElementById("benevolent-image").style.display = "block";
-      comment.innerHTML = "Artist is feeling benevolent."
-
+      comment.innerHTML = "Artist is feeling benevolent.";
     } else if (mood === "happy") {
       document.getElementById("happy-image").style.display = "block";
-      comment.innerHTML = "Artist is happy."
+      comment.innerHTML = "Artist is happy.";
+    } else if (mood === "impatient") {
+      document.getElementById("impatient-image").style.display = "block";
+      comment.innerHTML = "Artist is impatient. If you don't do something interesting soon his mood will change.";
     }
-  }
-  
-  function getHandsData(results) {
+}
+
+function getHandsData(results) {
     predictions = results;
-  }
-  
-  function draw() {
+}
+
+function draw() {
     background(255, 255, 255);
     translate(video.width, 0);
     scale(-1, 1);
     image(video, 0, 0, 740, 580);
-  
+
     for (let hand of predictions) {
-      const keypoints = hand.keypoints;
-      
-      for (let i = 0; i < keypoints.length; i++) {
-        const keypoint = keypoints[i];
-        
-        push();
-        noStroke();
-  
-        if (i >= 9 && i <= 12) {
-          fill(255, 0, 0); 
-        } else {
-          fill(0, 255, 0);
+        const keypoints = hand.keypoints;
+        for (let keypoint of keypoints) {
+            push();
+            noStroke();
+            fill(0, 255, 0);
+            ellipse(keypoint.x, keypoint.y, 10);
+            pop();
         }
-  
-        ellipse(keypoint.x, keypoint.y, 10);
-        pop();
-      }
     }
+
+    filterObject();
+    displayFilter();
+}
+
+
+
+function filterObject() {
+  const facialFeatures = {
+    positionX: 100,
+    positionY: 100,
+    filterSize: 20,
+  };
+
+  randomFacialFeatures = Math.floor(Math.random() * 3);
+}
+
+function displayFilter() {
+  for (let feature of happyFilterShowing) {
+    image(
+      happyFilterArray[randomFacialFeatures],
+      feature.positionX,
+      feature.positionY,
+      feature.filterSize,
+    );
   }
-  
+}
+
 
 function modelLoaded() {
     console.log("Model Loaded!");
